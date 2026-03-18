96b00137742b0df7e82cd380bf2035e45c09f722
imthiazRagib <imthiazragib@gmail.com>
2026-03-19 01:32:32 +0400

    Enhance routing logic with authentication checks in GoRouter
    - Added authentication checks in the GoRouter to redirect users based on their login status.
    - Implemented redirects to the dashboard for logged-in users accessing login/signup routes and to the login page for unauthenticated users trying to access protected routes.
    - Improved code readability by consolidating route definitions.

--- lib/router.dart
+++ lib/router.dart
@@ -1,3 +1,4 @@
+import 'package:assignment_customer_app/core/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:assignment_customer_app/features/auth/login.dart';
 import 'package:assignment_customer_app/features/auth/signup.dart';
@@ -6,35 +7,40 @@ import 'package:assignment_customer_app/features/order_page/order_page.dart';
 import 'package:assignment_customer_app/features/profile.dart';
 import 'package:assignment_customer_app/widgets/floating_nav_bar.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'features/landing/landing.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/',
   routes: [
     ShellRoute(
-      builder: (context, state, child) => Stack(
-        children: [
-          child,
-          const FloatingNavBar(),
-        ],
-      ),
+      builder: (context, state, child) =>
+          Stack(children: [child, const FloatingNavBar()]),
+      redirect: (context, state) {
+        final authProvider = context.read<AuthProvider>();
+        final loggedIn = authProvider.isLoggedIn;
+
+        final path = state.matchedLocation;
+        final isLoginRoute = path == '/login' || path == '/signup';
+        final isProtectedRoute = path == '/dashboard' || path == '/profile';
+        
+        if (loggedIn && isLoginRoute) {
+          return '/dashboard';
+        }
+        if (!loggedIn && isProtectedRoute) {
+          return '/login';
+        }
+
+        return null;
+      },
       routes: [
-        GoRoute(
-          path: '/',
-          builder: (context, state) => const LandingPage(),
-        ),
-        GoRoute(
-          path: '/login',
-          builder: (context, state) => const LoginPage(),
-        ),
+        GoRoute(path: '/', builder: (context, state) => const LandingPage()),
+        GoRoute(path: '/login', builder: (context, state) => const LoginPage()),
         GoRoute(
           path: '/signup',
           builder: (context, state) => const SignUpPage(),
         ),
-        GoRoute(
-          path: '/order',
-          builder: (context, state) => const OrderPage(),
-        ),
+        GoRoute(path: '/order', builder: (context, state) => const OrderPage()),
         GoRoute(
           path: '/dashboard',
           builder: (context, state) => const DashboardPage(),
